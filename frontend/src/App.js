4c5dd7e21911162dcfe6667ff935dbd15caf2615
uzairhyd <uzairhyd89@gmail.com>
2025-06-07 15:00:54 +0530

    Added the header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        welcome to the React App!
+        <Header />
+          Welcome to the proshop
+        <Footer />
       </div>
     );
   }
