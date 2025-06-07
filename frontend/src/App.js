a04153960d1b06d4aea8cd5e0494a8956e4e2fd5
uzairhyd <uzairhyd89@gmail.com>
2025-06-07 16:06:05 +0530

    updated the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import { Container } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-          Welcome to the proshop
+        <main className='py-3'>
+         <Container>
+            <h1>Welcome to the React App</h1>
+          </Container>
+        </main>
         <Footer />
       </div>
     );
