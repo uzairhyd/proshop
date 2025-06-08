67347aedcb62938af1bcf0a98f105999f6660945
uzairhyd <uzairhyd89@gmail.com>
2025-06-08 16:09:48 +0530

    added the data for the products

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+
+from .serializer import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -12,14 +16,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serialzier = ProductSerializer(products, many=True)
+    return Response(serialzier.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serialzier = ProductSerializer(product, many=False)
+    return Response(serialzier.data)
