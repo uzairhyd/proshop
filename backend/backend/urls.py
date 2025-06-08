9cf454c0117669bc55c6e7a692f4b12d25e03d78
uzairhyd <uzairhyd89@gmail.com>
2025-06-08 15:20:31 +0530

    Added the image field

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),  # Include the base app's URLs
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
