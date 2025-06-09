9b4cfe3ff6dff8c8ecf0b2755de8c60ab93e33e6
uzairhyd <uzairhyd89@gmail.com>
2025-06-09 08:45:19 +0530

    Adding addtocart feature

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,22 @@ import {
         productListReducer,
         productDetailsReducer } from './reducers/productReducers';
 
+import { cartReducer } from './reducers/cartReducers';
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
 const store = configureStore({
   reducer: {
     // Add your reducers here
     productList: productListReducer,
-    productDetails: productDetailsReducer, // Product list reducer
+    productDetails: productDetailsReducer, 
+    cart: cartReducer, 
   },
-    preloadedState: {}, // Initial state can be set here if needed
+    preloadedState: {
+        cart: { cartItems: cartItemsFromStorage },
+    }, // Initial state can be set here if needed
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Disable serializable check for non-serializable data
