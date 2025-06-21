c62b7521db2cf72c7ecdd0654aaf6999ac5a6434
uzairhyd <uzairhyd89@gmail.com>
2025-06-21 16:35:41 +0530

    Order View and URL

---
+++ backend/base/views/order_views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from rest_framework.response import Response
+
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.products import products
+
+from base.serializer import ProductSerializer, OrderSerializer
+
+
+from rest_framework import status
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No order items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        #(1)Create order
+        order = Order.objects.create(
+            user=user,
+            payment_method=data['paymentMethod'],
+            tax_price=data['taxPrice'],
+            shipping_price=data['shippingPrice'],
+            total_price=data['totalPrice']
+        )
+        #(2)Create shipping address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postal_code=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+        #(3)Create order items and set the order to orderItem relationship
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product'])
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+
+            #(4)Update stock of products
+
+            product.count_in_stock -= item.qty
+            product.save()
+        
+
+        return Response('ORDER')
+
+        for item in orderItems:
+            product = Product.objects.get(id=item['product'])
+            orderItem = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=item['qty'],
+                price=item['price'],
+                image=product.image.url,
+            )
+            product.count_in_stock -= item['qty']
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
