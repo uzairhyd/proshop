f85b02189c19750fcf79e5d57176ad9110ea4270
uzairhyd <uzairhyd89@gmail.com>
2025-08-15 11:32:50 +0530

    Added hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 import './index.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </Provider>
 );
